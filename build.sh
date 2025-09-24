d909a653839d0c57133221ab0e1f499ef27d6ed9
febin thomas <tfebin39@gmail.com>
2025-09-24 16:40:55 +0530

    commit

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-apt-get update
-apt-get install -y chromium chromium-driver
+# #!/bin/bash
+# apt-get update
+# apt-get install -y chromium chromium-driver
+# pip install -r requirements.txt
+
+apt-get update -y
+apt-get install -y chromium-browser
 pip install -r requirements.txt
