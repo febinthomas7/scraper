72a3d60e2e66a0435ad52cc63d309b4a9c448778
febin thomas <tfebin39@gmail.com>
2025-09-24 15:46:23 +0530

    commit

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from fastapi import FastAPI
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
 
 
 app = FastAPI()
 
+@app.get("/")
+def hello():
+    return {"message": "Hello, World!"}
+
 @app.get("/scrape")
 def scrape_who_news():
     try:
-     # Selenium options
         chrome_options = Options()
-        chrome_options.binary_location = "/usr/bin/chromium"  # for Render
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--no-sandbox")  # needed on Linux servers like Render
         driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()), 
             options=chrome_options
